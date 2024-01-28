2d4efb089d6ec666e7c5bee5d07f7ee6a3794a19
yere <yeremia@aimsis.com>
2024-01-28 22:25:39 +0700

    add, edit, and also commenting line that i think i dont wanna use in my portfolio

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var audio = new Audio('assets/sentmessage.mp3');
-var contactString = "<div class='social'> <a target='_blank' href='tel:+918600765857'> <div class='socialItem' id='call'><img class='socialItemI' src='images/phone.svg'/><label class='number'>8600765857</label></div> </a> <a href='mailto:patilvinu777@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/Vinayak-09'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a> <a target='_blank' href='https://wa.me/918600765857'> <div class='socialItem'><img class='socialItemI' src='images/whatsapp.svg' alt=''></div> </a> <a target='_blank' href='https://t.me/vinayak_09'> <div class='socialItem'><img class='socialItemI' src='images/telegram.svg' alt=''></div> </a> <a target='_blank' href='https://instagram.com/vinayak_patil_09'> <div class='socialItem'><img class='socialItemI' src='images/instagram.svg' alt=''> </div> </a> <a href='https://www.linkedin.com/in/vinayak-patil-793bb5206/' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
+var contactString = "<div class='social'><a href='mailto:yeremiayrobert@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/yeremia15'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a> <a target='_blank' href='https://wa.me/6281286717921'> <div class='socialItem'><img class='socialItemI' src='images/whatsapp.svg' alt=''></div> <a href='https://www.linkedin.com/in/yeremia-yosia/' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
 var resumeString = "<img src='images/yere_thumbnail.png' class='resumeThumbnail'><div class='downloadSpace'><div class='pdfname'><img src='images/pdf.png'><label>Yeremia Resume.pdf</label></div><a href='assets/yeremia_yosia_cv.pdf' download='yeremia_yosia_cv.pdf'><img class='download' src='images/downloadIcon.svg'></a></div>";
-var addressString = "<div class='mapview'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238.63833262443757!2d74.19014864534314!3d16.865338763272877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1a7dcf40f5dd7%3A0xd7b69fe1fcfa9877!2zMTbCsDUxJzU1LjQiTiA3NMKwMTEnMjUuMyJF!5e0!3m2!1sen!2sin!4v1645079906766!5m2!1sen!2sin' class='map'></iframe></div><label class='add'><address>B2 'Asara'<br>Kodoli<br>Kolhapur, Maharashtra, INDIA 416114</address>";
+// var addressString = "<div class='mapview'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238.63833262443757!2d74.19014864534314!3d16.865338763272877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1a7dcf40f5dd7%3A0xd7b69fe1fcfa9877!2zMTbCsDUxJzU1LjQiTiA3NMKwMTEnMjUuMyJF!5e0!3m2!1sen!2sin!4v1645079906766!5m2!1sen!2sin' class='map'></iframe></div><label class='add'><address>B2 'Asara'<br>Kodoli<br>Kolhapur, Maharashtra, INDIA 416114</address>";
+
 function startFunction() {
     setLastSeen();
     waitAndResponce("intro");
@@ -61,7 +62,9 @@ function sendMsg() {
     document.getElementById("listUL").appendChild(myLI);
     var s = document.getElementById("chatting");
     s.scrollTop = s.scrollHeight;
-    setTimeout(function () { waitAndResponce(ti) }, 1500);
+    setTimeout(function () {
+        waitAndResponce(ti)
+    }, 1500);
     input.value = "";
     playSound();
 }
@@ -72,31 +75,35 @@ function waitAndResponce(inputText) {
     switch (inputText.toLowerCase().trim()) {
         case "intro":
             setTimeout(() => {
-                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>Yeremia Yosia Robert</a>.</span><br><br>Currently, I am employed as an IT Project Coordinator at PT Electronic Data Interchange Indonesia (EDII). I have a strong passion for various aspects of Computer Systems & Engineering, particularly in Coding & Database<br><br>I am enthusiastic about pursuing a career that allows me to apply my knowledge and experiences while further expanding my skills in a dynamic and stable work environment. <br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
+                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>Yeremia Yosia Robert</a>.</span><br><br>Currently, I am employed as an IT Project Coordinator at PT Electronic Data Interchange Indonesia (EDII) for handling project : <br> Maintenance and Enhancement of Customs Excise Information System Automation 4.0 (CEISA 4.0) with PROSPERA. <br><br>I have a strong passion for various aspects of Computer Systems & Engineering, particularly in Coding & Database<br><br>I am enthusiastic about pursuing a career that allows me to apply my knowledge and experiences while further expanding my skills in a dynamic and stable work environment. <br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
             }, 2000);
             break;
         case "help":
-            sendTextMessage("<span class='sk'>Send Keyword to get what you want to know about me...<br>e.g<br><span class='bold'>'skills'</span> - to know my skills<br><span class='bold'>'resume'</span> - to get my resume<br><span class='bold'>'education'</span> - to get my education details<br><span class='bold'>'address'</span> - to get my address<br><span class='bold'>'contact'</span> - to get ways to connect with me<br><span class='bold'>'projects'</span> - to get details of my projects<br><span class='bold'>'clear'</span> - to clear conversation<br><span class='bold'>'about'</span> - to know about this site</span>");
+            sendTextMessage("<span class='sk'>Send Keyword to get what you want to know about me...<br>e.g<br><span class='bold'>'skills'</span> - to know my skills<br><span class='bold'>'resume'</span> - to get my resume<br><span class='bold'>'experiences'</span> - to get my experience details<br><span class='bold'>'education'</span> - to get my education details<br><span class='bold'>'contact'</span> - to get ways to connect with me<br><span class='bold'>'projects'</span> - to get details of my projects<br><span class='bold'>'clear'</span> - to clear conversation<br><span class='bold'>'about'</span> - to know about this site</span>");
             break;
         case "resume":
             sendTextMessage(resumeString);
             break;
         case "skills":
-            sendTextMessage("<span class='sk'>I am currently pursuing B.Tech degree in Computer Science Engineering.<br><br>I can comfortably write code in following languages :<br><span class='bold'>Java<br>C++<br>C<br>PHP<br>Kotlin<br>Dart<br>Python<br>CSS<br>HTML</span><br><br>I've experiance with following frameworks :<span class='bold'><br>Android<br>Flutter<br>ReactJs<br>GTK</span><br><br>I use <span class='bold'>Arch Linux</span> as daily driver on my HP Pavilion 15-ec0xxx<br>OS:Arch Linux<br>DE:Gnome(More often) Kde(often)<br>Favourite IDE:VSCode</span>");
+            sendTextMessage("<span class='sk'>I can comfortably write code & queries using this following languages :<br><span class='bold'>Javascript<br>C++<br>PHP<br>Python<br>CSS<br>HTML<br>MySQL</span><br><br>I've experience with following frameworks :<span class='bold'><br>CodeIgniter<br>Django<br>Bootstrap</span><br><br>Not only in the technical side, but also i had a lot of another skills, such as : <span class='bold'><br>Microsoft Office & Google Documents (Docs, Sheets, Slides)<br>Figma<br>BPMN<br>Diagram(Flowchart, EER, Sequence, etc)<br>Git, Github, Gitlab, Bitbucket<br>NGINX<br>Google Cloud Platform (Compute Engine, Storage, SQL, etc)<br>Project Management (Agile Method)<br>Product Management</span><br><br>I usually use <span class='bold'>Windows OS</span> as daily driver on my own computer but also using <span class='bold'>Linux Ubuntu</span> too at some point in my laptop<br>OS: <span class='bold'>Windows 10</span> & <span class='bold'>Ubuntu 20</span><br>Favourite IDE: <span class='bold'>VSCode</span></span>");
             break;
 
+        case "experiences":
+            sendTextMessage("PT Electronic Data Interchanges Indonesia (EDII)<br>Location : Jakarta, Indonesia<br>Position : IT Project Coordinator (Jun 2023 - Now)<br><br>PT TER Solusindo (AIMSIS)<br>Location : Jakarta, Indonesia<br>Position : <br>Project Manager (Feb 2022 - Jun 2023)<br>Support Engineer (Dec 2021 - Feb 2022)<br>System Consultant & TS (Aug 2020 - Dec 2021)");
+            break;
+            
         case "education":
-            sendTextMessage("I am currently pusuing B.Tech degree in Computer Science Engineering from TKIET Kolhapur<br>Passing Year : 2023<br><br>I have completed my Diploma from Government Polytechnic Karad<br>Passing Year:2020<br>Result:86.06%<br><br>I have completed my Secondary school from local school known as SWV<br>Passing Year:2016");
+            sendTextMessage("I already graduate from the University of Esa Unggul with a major in Information Systems from the Faculty of Computer Science<br>Passing Year : 2023<br>Result in GPA (IPK): 3.70/4.00");
             break;
 
-        case "address":
-            sendTextMessage(addressString);
-            break;
+        // case "address":
+        //     sendTextMessage(addressString);
+        //     break;
         case "clear":
             clearChat();
             break;
         case "about":
-            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript from SCRATCH!<br><br>👨🏻‍💻 Designed and Developed by <a class='alink' target='_blank' href='https:\/\/instagram.com/vinayak_patil_09/'><span class='bold'>Vinayak Patil</a> with ❤️</span>");
+            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript<br><br>👨🏻‍💻 Developed by <a class='alink' target='_blank' href='https://www.linkedin.com/in/yeremia-yosia/'><span class='bold'>Yeremia Yosia Robert</a></span>");
             break;
         case "contact":
             sendTextMessage(contactString);
